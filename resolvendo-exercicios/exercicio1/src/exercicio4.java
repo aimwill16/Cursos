566ae55d02a3c15c30e8152ac8def50c8f42d2ba
aimwill16 <williancosta16@gmail.com>
2023-04-11 18:08:14 -0300

    exercicio 4

--- resolvendo-exercicios/exercicio1/src/exercicio4.java
+++ resolvendo-exercicios/exercicio1/src/exercicio4.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class exercicio4 {
+    /*
+    Faça um programa que peça N números inteiros
+    calcule e mostre a quantidade de números pares
+    e a quantidade de números impares. */
     public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        int numero, par=0, impar=0;
+
+        do {
+            System.out.println("Digite um número:");
+            numero = x.nextInt();
+            if (numero % 2 == 0){
+                par+= 1;
+            } else {
+                impar+=1;
+            }
+            System.out.println("Quantidade de números pares: " +par);
+            System.out.println("Quantidade de números impares: " +impar);
+        }while(impar<2 || par<2);
+
+        x.close();
 
     }
 }
